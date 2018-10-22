af78dca364e69eeed29db01b5959875d28989623
kevin <kevin.pardo.2016@upb.edu.co>
2018-10-22 14:46:38 -0500

    faltan notas complete v2

---
+++ Notapp/faltanotas.php
@@ -0,0 +1,52 @@
+<html>
+<head>
+	<title>Notas Matriculadas:</title>
+
+</head>
+<body>
+	<h1>Notas matriculadas: 332799</h1>
+	<table border="1">
+		<tr>
+			<td>Id:</td>
+			<td>NRC:</td>
+			<td>Seguimiento1:</td>
+			<td>Parcial1:</td>
+			<td>Seguimiento2:</td>
+			<td>Parcial2:</td>
+			<td>Laboratorio:</td>
+		</tr>
+		
+		
+		<tr>
+			<td>0</td>
+			<td>64390</td>
+			<td>5</td>
+			<td>4</td>
+			<td>4</td>
+			<td>4</td>
+			<td bgcolor="#00FF00">-2</td>
+		</tr>
+		
+		<tr>
+			<td>2</td>
+			<td>45238</td>
+			<td>5</td>
+			<td>4</td>
+			<td>5</td>
+			<td>4</td>
+			<td>4</td>
+		</tr>
+		
+		<tr>
+			<td>3</td>
+			<td>64628</td>
+			<td>5</td>
+			<td>5</td>
+			<td>5</td>
+			<td>5</td>
+			<td>5</td>
+		</tr>
+			</table>
+	<a href="cerrar_session.php"> cerrar sesión</a> 
+	</body>
+</html
